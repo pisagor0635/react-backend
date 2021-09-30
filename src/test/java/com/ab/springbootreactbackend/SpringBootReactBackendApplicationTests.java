81e2e2d406f314e24c93bf69872dae971b0f2705
pisagor0635 <adnan_bilgen@hotmail.com>
2021-09-30 17:27:31 +0300

    first commit

---
+++ src/test/java/com/ab/springbootreactbackend/SpringBootReactBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ab.springbootreactbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
