a72e3f434a5e021fba4caa129cc71ed18d48484e
Adnan Bilgen <adnan_bilgen@hotmail.com>
2021-10-04 23:27:09 +0300

    validation works

--- src/main/java/com/ab/springbootreactbackend/controller/UserController.java
+++ src/main/java/com/ab/springbootreactbackend/controller/UserController.java
@@ -1,10 +1,17 @@
 package com.ab.springbootreactbackend.controller;
 
+import com.ab.springbootreactbackend.error.ApiError;
 import com.ab.springbootreactbackend.model.User;
 import com.ab.springbootreactbackend.service.UserService;
+import com.ab.springbootreactbackend.util.GenericResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -15,7 +22,17 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/create")
-    public void createUser(@RequestBody User user) {
+    public ResponseEntity<?> createUser(@RequestBody User user) {
+        String userName = user.getUsername();
+        if (userName == null || userName.length() == 0) {
+            ApiError error = new ApiError(400, "Validation Error", "api/v1/users/create");
+            Map<String, String> validationErrors = new HashMap<>();
+            validationErrors.put("username", "username cannot be null");
+            error.setValidationErrors(validationErrors);
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+        }
         userService.createUser(user);
+        return ResponseEntity.ok(new GenericResponse("user created"));
     }
 }
