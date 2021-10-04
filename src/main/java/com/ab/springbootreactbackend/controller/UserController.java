69f803de36fecbdf62cff924e603eedb54027175
Adnan Bilgen <adnan_bilgen@hotmail.com>
2021-10-05 00:06:20 +0300

    validation works

--- src/main/java/com/ab/springbootreactbackend/controller/UserController.java
+++ src/main/java/com/ab/springbootreactbackend/controller/UserController.java
@@ -23,15 +23,24 @@ public class UserController {
 
     @PostMapping("/create")
     public ResponseEntity<?> createUser(@RequestBody User user) {
+
+        ApiError error = new ApiError(400, "Validation Error", "api/v1/users/create");
+        Map<String, String> validationErrors = new HashMap<>();
         String userName = user.getUsername();
         if (userName == null || userName.length() == 0) {
-            ApiError error = new ApiError(400, "Validation Error", "api/v1/users/create");
-            Map<String, String> validationErrors = new HashMap<>();
             validationErrors.put("username", "username cannot be null");
             error.setValidationErrors(validationErrors);
+        }
+        String displayName = user.getDisplayName();
+        if (displayName == null || displayName.length() == 0) {
+            validationErrors.put("displayName", "cannot be null");
+            error.setValidationErrors(validationErrors);
+        }
 
+        if (validationErrors.size() > 0) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
         }
+
         userService.createUser(user);
         return ResponseEntity.ok(new GenericResponse("user created"));
     }
